8ba8229edb1f7a92f76d2ed73089f2efa83499df
Mark Perryman <mark.perryman@metaswitch.com>
2021-12-06 08:21:47 +0000

    Format

--- src/bitaccumulator.rs
+++ src/bitaccumulator.rs
@@ -4,7 +4,6 @@ pub struct DiagsReport {
 }
 
 impl DiagsReport {
-
     pub fn new(digit_count: usize, values: Vec<usize>) -> DiagsReport {
         DiagsReport {
             digit_count: digit_count,
@@ -21,13 +20,14 @@ impl DiagsReport {
     }
 
     fn accumulate_bits(&self) -> BitAccumulator {
-        self.values.iter().fold(
-            BitAccumulator::zeroed(self.digit_count),
-            |ba, bits| ba.accumulate(*bits)
-        )
+        self.values
+            .iter()
+            .fold(BitAccumulator::zeroed(self.digit_count), |ba, bits| {
+                ba.accumulate(*bits)
+            })
     }
 
-    fn iter_values(&self) -> impl Iterator<Item=&usize> {
+    fn iter_values(&self) -> impl Iterator<Item = &usize> {
         self.values.iter()
     }
 
@@ -40,42 +40,45 @@ impl DiagsReport {
     }
 
     pub fn oxygen_rate(&self) -> usize {
-        (0..self.digit_count).rev().fold::<DiagsReport, _>(
-            self.cloned(),
-            |filtered_report, index| {
+        (0..self.digit_count)
+            .rev()
+            .fold::<DiagsReport, _>(self.cloned(), |filtered_report, index| {
                 let ba = filtered_report.accumulate_bits();
                 if filtered_report.values.len() == 1 {
                     DiagsReport::new(index, filtered_report.values)
                 } else {
                     DiagsReport::from_iter(
                         index,
-                        filtered_report.iter_values().filter(
-                            |v| bit_match_at_index(index, ba.most_common_bits(), **v)
-                        ).cloned())
+                        filtered_report
+                            .iter_values()
+                            .filter(|v| bit_match_at_index(index, ba.most_common_bits(), **v))
+                            .cloned(),
+                    )
                 }
-            }
-        ).values[0]
+            })
+            .values[0]
     }
 
     pub fn co2_scrub_rate(&self) -> usize {
-        (0..self.digit_count).rev().fold::<DiagsReport, _>(
-            self.cloned(),
-            |filtered_report, index| {
+        (0..self.digit_count)
+            .rev()
+            .fold::<DiagsReport, _>(self.cloned(), |filtered_report, index| {
                 let ba = filtered_report.accumulate_bits();
                 if filtered_report.values.len() == 1 {
                     DiagsReport::new(index, filtered_report.values)
                 } else {
                     DiagsReport::from_iter(
                         index,
-                        filtered_report.iter_values().filter(
-                            |v| bit_match_at_index(index, ba.least_common_bits(), **v)
-                        ).cloned())
+                        filtered_report
+                            .iter_values()
+                            .filter(|v| bit_match_at_index(index, ba.least_common_bits(), **v))
+                            .cloned(),
+                    )
                 }
-            }
-        ).values[0]
+            })
+            .values[0]
     }
 }
-
 
 #[derive(PartialEq, Debug)]
 pub struct BitAccumulator {
@@ -92,7 +95,9 @@ impl BitAccumulator {
     }
 
     pub fn accumulate(&self, bits: usize) -> BitAccumulator {
-        let new_bitcount: Vec<usize> = self.bitcount.iter()
+        let new_bitcount: Vec<usize> = self
+            .bitcount
+            .iter()
             .enumerate()
             .map(|(i, bc)| bc + ((bits & (1 << i)) >> i))
             .collect();
@@ -104,23 +109,19 @@ impl BitAccumulator {
     }
 
     pub fn most_common_bits(&self) -> usize {
-        self.bitcount.iter().enumerate()
-            .map(|(i, bc)| if 2 * bc >= self.count {
-                1 << i
-            } else {
-                0
-            })
-        .sum()
+        self.bitcount
+            .iter()
+            .enumerate()
+            .map(|(i, bc)| if 2 * bc >= self.count { 1 << i } else { 0 })
+            .sum()
     }
 
     pub fn least_common_bits(&self) -> usize {
-        self.bitcount.iter().enumerate()
-            .map(|(i, bc)| if 2 * bc >= self.count {
-                0
-            } else {
-                1 << i
-            })
-        .sum()
+        self.bitcount
+            .iter()
+            .enumerate()
+            .map(|(i, bc)| if 2 * bc >= self.count { 0 } else { 1 << i })
+            .sum()
     }
 }
 
@@ -129,31 +130,22 @@ fn bit_match_at_index(index: usize, value_one: usize, value_two: usize) -> bool 
     (value_one & bit_mask) == (value_two & bit_mask)
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn check_diags_rates() {
-        let dg = DiagsReport::new(5, vec![
-            0b00100,
-            0b11110,
-            0b10110,
-            0b10111,
-            0b10101,
-            0b01111,
-            0b00111,
-            0b11100,
-            0b10000,
-            0b11001,
-            0b00010,
-            0b01010,
-        ]);
+        let dg = DiagsReport::new(
+            5,
+            vec![
+                0b00100, 0b11110, 0b10110, 0b10111, 0b10101, 0b01111, 0b00111, 0b11100, 0b10000,
+                0b11001, 0b00010, 0b01010,
+            ],
+        );
         assert_eq!(dg.gamma_rate(), 22);
         assert_eq!(dg.epsilon_rate(), 9);
         assert_eq!(dg.oxygen_rate(), 23);
         assert_eq!(dg.co2_scrub_rate(), 10);
     }
-
 }
